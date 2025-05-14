5aa5ad094988e3d7bb4014dd0c0258a2ed4b8820
Ilham593 <sosham127@gmail.com>
2025-05-14 04:50:23 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cerita-app/sw.js', { scope: '/cerita-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
